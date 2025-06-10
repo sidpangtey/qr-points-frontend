363ca4b123adefe8caf6914263d2433a8e69c839
sidpangtey <siddharth.pangtey@scanova.io>
2025-06-10 17:05:45 +0530

    Added useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useContext } from 'react';
+import React, { useState, useEffect, createContext, useContext } from 'react';
 
 // Context definition
 const AppContext = createContext({
